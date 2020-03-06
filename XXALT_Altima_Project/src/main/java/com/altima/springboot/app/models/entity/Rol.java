74929cc3e39e78d294e801e2b816078093ef3f00
kamudoce <59659640+kamudoce@users.noreply.github.com>
2020-03-06 12:37:42 -0600

    Index, Catalogo

--- XXALT_Altima_Project/src/main/java/com/altima/springboot/app/models/entity/Rol.java
+++ XXALT_Altima_Project/src/main/java/com/altima/springboot/app/models/entity/Rol.java
@@ -4,8 +4,13 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+
+import org.hibernate.annotations.GenericGenerator;
 
 
 @Entity
@@ -17,7 +22,9 @@ public class Rol implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO, generator="native")
+	@GenericGenerator(name="native",strategy="native")
 	@Column(name="id_rol")
 	@NotBlank
 	private Long idRol;
--- XXALT_Altima_Project/src/main/java/com/altima/springboot/app/models/entity/Usuario.java
+++ XXALT_Altima_Project/src/main/java/com/altima/springboot/app/models/entity/Usuario.java
@@ -4,8 +4,13 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name = "xxalt_usuario")
@@ -16,7 +21,9 @@ public class Usuario  implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO, generator="native")
+	@GenericGenerator(name="native",strategy="native")
 	@Column(name="id_usuario")
 	@NotBlank
 	private Long idUsuario;
