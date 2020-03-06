3a689b6a4d133a69b6fe4f0d3ded15ec9a877d12
Do <Do@DESKTOP-AVK6DCT>
2020-03-06 17:21:07 -0600

    Add HashCode MaterialPrenda

--- XXALT_Altima_Project/src/main/java/com/altima/springboot/app/models/entity/MaterialPrenda.java
+++ XXALT_Altima_Project/src/main/java/com/altima/springboot/app/models/entity/MaterialPrenda.java
@@ -48,4 +48,111 @@ public class MaterialPrenda implements Serializable{
 	@Column(name="ultima_fecha_modificacion")
 	@NotBlank
 	private String ultimaFechaModificacion;
+
+	public Long getIdLookup() {
+		return idLookup;
+	}
+
+	public void setIdLookup(Long idLookup) {
+		this.idLookup = idLookup;
+	}
+
+	public String getIdText() {
+		return idText;
+	}
+
+	public void setIdText(String idText) {
+		this.idText = idText;
+	}
+
+	public String getCreadoPor() {
+		return creadoPor;
+	}
+
+	public void setCreadoPor(String creadoPor) {
+		this.creadoPor = creadoPor;
+	}
+
+	public String getActualizadoPor() {
+		return actualizadoPor;
+	}
+
+	public void setActualizadoPor(String actualizadoPor) {
+		this.actualizadoPor = actualizadoPor;
+	}
+
+	public String getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(String fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public String getUltimaFechaModificacion() {
+		return ultimaFechaModificacion;
+	}
+
+	public void setUltimaFechaModificacion(String ultimaFechaModificacion) {
+		this.ultimaFechaModificacion = ultimaFechaModificacion;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((actualizadoPor == null) ? 0 : actualizadoPor.hashCode());
+		result = prime * result + ((creadoPor == null) ? 0 : creadoPor.hashCode());
+		result = prime * result + ((fechaCreacion == null) ? 0 : fechaCreacion.hashCode());
+		result = prime * result + ((idLookup == null) ? 0 : idLookup.hashCode());
+		result = prime * result + ((idText == null) ? 0 : idText.hashCode());
+		result = prime * result + ((ultimaFechaModificacion == null) ? 0 : ultimaFechaModificacion.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MaterialPrenda other = (MaterialPrenda) obj;
+		if (actualizadoPor == null) {
+			if (other.actualizadoPor != null)
+				return false;
+		} else if (!actualizadoPor.equals(other.actualizadoPor))
+			return false;
+		if (creadoPor == null) {
+			if (other.creadoPor != null)
+				return false;
+		} else if (!creadoPor.equals(other.creadoPor))
+			return false;
+		if (fechaCreacion == null) {
+			if (other.fechaCreacion != null)
+				return false;
+		} else if (!fechaCreacion.equals(other.fechaCreacion))
+			return false;
+		if (idLookup == null) {
+			if (other.idLookup != null)
+				return false;
+		} else if (!idLookup.equals(other.idLookup))
+			return false;
+		if (idText == null) {
+			if (other.idText != null)
+				return false;
+		} else if (!idText.equals(other.idText))
+			return false;
+		if (ultimaFechaModificacion == null) {
+			if (other.ultimaFechaModificacion != null)
+				return false;
+		} else if (!ultimaFechaModificacion.equals(other.ultimaFechaModificacion))
+			return false;
+		return true;
+	}
 }
