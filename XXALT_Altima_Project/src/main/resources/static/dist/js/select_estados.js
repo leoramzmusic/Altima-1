0bc1b62d154397c5250887a0ce0ba49f6d5b6b99
kamudoce <59659640+kamudoce@users.noreply.github.com>
2020-03-12 10:31:40 -0600

    Cliente agregar

--- XXALT_Altima_Project/src/main/resources/static/dist/js/select_estados.js
+++ XXALT_Altima_Project/src/main/resources/static/dist/js/select_estados.js
@@ -26,3 +26,32 @@ $(document).ready(function(){
     })
     .trigger( "change" );
 });
+
+$(document).ready(function(){
+    // Cargamos los estados
+    var estados = "<option value='' disabled selected>Selecciona el estado</option>";
+
+    for (var key in municipios) {
+        if (municipios.hasOwnProperty(key)) {
+            estados = estados + "<option value='" + key + "'>" + key + "</option>";
+        }
+    }
+
+    $('#estado2').html(estados);
+
+    // Al detectar
+    $( "#estado2" ).change(function() {
+        var html = "<option value='' disabled selected>Selecciona el municipio</option>";
+        $( "#estado2 option:selected" ).each(function() {
+            var estado = $(this).text();
+            if(estado != "Selecciona el estado"){
+                var municipio = municipios[estado];
+                for (var i = 0; i < municipio.length; i++)
+                    html += "<option value='" + municipio[i] + "'>" + municipio[i] + "</option>";
+            }
+        });
+        $('#municipio2').html(html);
+        $('select2').material_select('update');
+    })
+    .trigger( "change" );
+});
