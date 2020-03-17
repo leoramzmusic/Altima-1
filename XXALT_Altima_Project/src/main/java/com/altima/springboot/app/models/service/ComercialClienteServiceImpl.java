bf8a92acffdf5e61452375b41eae7d6848e64371
TIErik <55719391+TIErik@users.noreply.github.com>
2020-03-17 13:43:45 -0600

    Add files via upload

---
+++ XXALT_Altima_Project/src/main/java/com/altima/springboot/app/models/service/ComercialClienteServiceImpl.java
@@ -0,0 +1,40 @@
+package com.altima.springboot.app.models.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.altima.springboot.app.models.entity.ComercialCliente;
+import com.altima.springboot.app.repository.ComercialClienteRepositoty;
+
+@Service
+public class ComercialClienteServiceImpl  implements IComercialClienteService{
+	
+	@Autowired
+	private ComercialClienteRepositoty repository;
+	@Override
+	@Transactional(readOnly=true)
+	public List<ComercialCliente> findAll() {
+		// TODO Auto-generated method stub
+		return (List<ComercialCliente>) repository.findAll();
+	}
+
+	@Override
+	public void save(ComercialCliente ComercialCliente) {
+		repository.save(ComercialCliente);
+		
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		repository.deleteById(id);
+	}
+
+	@Override
+	public ComercialCliente findOne(Long id) {
+		// TODO Auto-generated method stub
+		return repository.findById(id).orElse(null);
+	}
+
+}
