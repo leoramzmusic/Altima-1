c6f22db1426f77c5bd0edcfd917d32a45b96668f
DTI <DTI@DESKTOP-AVK6DCT>
2020-03-23 09:20:40 -0600

    Se agrego campo nombre a la taba de Tela

--- XXALT_Altima_Project/src/main/java/com/altima/springboot/app/models/entity/DisenioTela.java
+++ XXALT_Altima_Project/src/main/java/com/altima/springboot/app/models/entity/DisenioTela.java
@@ -64,6 +64,12 @@ public class DisenioTela implements Serializable{
 	@NotBlank
 	private String descripcionTela;
 	
+	@Column(name="nombre_tela")
+	@NotBlank
+	private String nombreTela;
+	
+	
+
 	@Column(name="linea_tela")
 	@NotBlank
 	private String lineaTela;
@@ -307,6 +313,14 @@ public class DisenioTela implements Serializable{
 	public String getDescripcion() {
 		return descripcion;
 	}
+	
+	public String getNombreTela() {
+		return nombreTela;
+	}
+
+	public void setNombreTela(String nombreTela) {
+		this.nombreTela = nombreTela;
+	}
 
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
@@ -320,22 +334,7 @@ public class DisenioTela implements Serializable{
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((actualizadoPor == null) ? 0 : actualizadoPor.hashCode());
-		result = prime * result + ((ancho == null) ? 0 : ancho.hashCode());
-		result = prime * result + ((claveTela == null) ? 0 : claveTela.hashCode());
-		result = prime * result + ((consumoPromedio == null) ? 0 : consumoPromedio.hashCode());
-		result = prime * result + ((creadoPor == null) ? 0 : creadoPor.hashCode());
-		result = prime * result + ((descripcionTela == null) ? 0 : descripcionTela.hashCode());
-		result = prime * result + ((existencia == null) ? 0 : existencia.hashCode());
-		result = prime * result + ((fechaCreacion == null) ? 0 : fechaCreacion.hashCode());
-		result = prime * result + ((idCalidad == null) ? 0 : idCalidad.hashCode());
-		result = prime * result + ((idFamiliaComposicion == null) ? 0 : idFamiliaComposicion.hashCode());
-		result = prime * result + ((idLookup == null) ? 0 : idLookup.hashCode());
-		result = prime * result + ((idTela == null) ? 0 : idTela.hashCode());
-		result = prime * result + ((idText == null) ? 0 : idText.hashCode());
-		result = prime * result + ((indicaciones == null) ? 0 : indicaciones.hashCode());
-		result = prime * result + ((lineaTela == null) ? 0 : lineaTela.hashCode());
-		result = prime * result + ((ultimaFechaModificacion == null) ? 0 : ultimaFechaModificacion.hashCode());
+		result = prime * result + ((nombreTela == null) ? 0 : nombreTela.hashCode());
 		return result;
 	}
 
@@ -348,85 +347,10 @@ public class DisenioTela implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		DisenioTela other = (DisenioTela) obj;
-		if (actualizadoPor == null) {
-			if (other.actualizadoPor != null)
+		if (nombreTela == null) {
+			if (other.nombreTela != null)
 				return false;
-		} else if (!actualizadoPor.equals(other.actualizadoPor))
-			return false;
-		if (ancho == null) {
-			if (other.ancho != null)
-				return false;
-		} else if (!ancho.equals(other.ancho))
-			return false;
-		if (claveTela == null) {
-			if (other.claveTela != null)
-				return false;
-		} else if (!claveTela.equals(other.claveTela))
-			return false;
-		if (consumoPromedio == null) {
-			if (other.consumoPromedio != null)
-				return false;
-		} else if (!consumoPromedio.equals(other.consumoPromedio))
-			return false;
-		if (creadoPor == null) {
-			if (other.creadoPor != null)
-				return false;
-		} else if (!creadoPor.equals(other.creadoPor))
-			return false;
-		if (descripcionTela == null) {
-			if (other.descripcionTela != null)
-				return false;
-		} else if (!descripcionTela.equals(other.descripcionTela))
-			return false;
-		if (existencia == null) {
-			if (other.existencia != null)
-				return false;
-		} else if (!existencia.equals(other.existencia))
-			return false;
-		if (fechaCreacion == null) {
-			if (other.fechaCreacion != null)
-				return false;
-		} else if (!fechaCreacion.equals(other.fechaCreacion))
-			return false;
-		if (idCalidad == null) {
-			if (other.idCalidad != null)
-				return false;
-		} else if (!idCalidad.equals(other.idCalidad))
-			return false;
-		if (idFamiliaComposicion == null) {
-			if (other.idFamiliaComposicion != null)
-				return false;
-		} else if (!idFamiliaComposicion.equals(other.idFamiliaComposicion))
-			return false;
-		if (idLookup == null) {
-			if (other.idLookup != null)
-				return false;
-		} else if (!idLookup.equals(other.idLookup))
-			return false;
-		if (idTela == null) {
-			if (other.idTela != null)
-				return false;
-		} else if (!idTela.equals(other.idTela))
-			return false;
-		if (idText == null) {
-			if (other.idText != null)
-				return false;
-		} else if (!idText.equals(other.idText))
-			return false;
-		if (indicaciones == null) {
-			if (other.indicaciones != null)
-				return false;
-		} else if (!indicaciones.equals(other.indicaciones))
-			return false;
-		if (lineaTela == null) {
-			if (other.lineaTela != null)
-				return false;
-		} else if (!lineaTela.equals(other.lineaTela))
-			return false;
-		if (ultimaFechaModificacion == null) {
-			if (other.ultimaFechaModificacion != null)
-				return false;
-		} else if (!ultimaFechaModificacion.equals(other.ultimaFechaModificacion))
+		} else if (!nombreTela.equals(other.nombreTela))
 			return false;
 		return true;
 	}
