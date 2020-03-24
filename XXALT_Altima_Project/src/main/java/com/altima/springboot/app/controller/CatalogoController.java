e776f95435955cd906184d1fad85dbc6c6fd5ee4
kamudoce <59659640+kamudoce@users.noreply.github.com>
2020-03-24 14:02:56 -0600

    Fix

--- XXALT_Altima_Project/src/main/java/com/altima/springboot/app/controller/CatalogoController.java
+++ XXALT_Altima_Project/src/main/java/com/altima/springboot/app/controller/CatalogoController.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -42,6 +43,11 @@ public class CatalogoController {
 	public List<DisenioLookup> accountSummary() {
 
 		return catalogo.findAllMarca();
+	}
+	
+	@GetMapping("/catalogo") 
+	public String agregarExpediente() {
+		return "catalogo";
 	}
 
 	@RequestMapping(value = { "/catalogos", "/catalogos-marcas" }, method = RequestMethod.GET)
