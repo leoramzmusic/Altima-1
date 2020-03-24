411f6055bcc3f63db29f00e38abf149244160ee5
DTI <DTI@DESKTOP-AVK6DCT>
2020-03-24 12:02:47 -0600

    se hizo la entidad muchos a muchos de prenda patronaje

---
+++ XXALT_Altima_Project/src/main/java/com/altima/springboot/app/models/entity/DisenioPrendaPatronaje.java
@@ -0,0 +1,155 @@
+package com.altima.springboot.app.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "alt_disenio_prenda_patronaje")
+public class DisenioPrendaPatronaje implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@Column(name="id_prenda_patronaje")
+	@GeneratedValue(strategy=GenerationType.AUTO, generator="native")
+	@GenericGenerator(name="native",strategy="native")
+	private Long idPrendaPatronaje;
+	
+	@Column(name="id_prenda")
+	@NotBlank
+	private Long idPrenda;
+	
+	@Column(name="id_patronaje")
+	@NotBlank
+	private String idPatronaje;
+	
+	@Column(name="cantidad_tela")
+	@NotBlank
+	private String cantidadTela;
+	
+	@Column(name="cantidad_forro")
+	@NotBlank
+	private String cantidadForro;
+	
+	@Column(name="cantidad_entretela")
+	@NotBlank
+	private String cantidadEntretela;
+
+	public Long getIdPrendaPatronaje() {
+		return idPrendaPatronaje;
+	}
+
+	public void setIdPrendaPatronaje(Long idPrendaPatronaje) {
+		this.idPrendaPatronaje = idPrendaPatronaje;
+	}
+
+	public Long getIdPrenda() {
+		return idPrenda;
+	}
+
+	public void setIdPrenda(Long idPrenda) {
+		this.idPrenda = idPrenda;
+	}
+
+	public String getIdPatronaje() {
+		return idPatronaje;
+	}
+
+	public void setIdPatronaje(String idPatronaje) {
+		this.idPatronaje = idPatronaje;
+	}
+
+	public String getCantidadTela() {
+		return cantidadTela;
+	}
+
+	public void setCantidadTela(String cantidadTela) {
+		this.cantidadTela = cantidadTela;
+	}
+
+	public String getCantidadForro() {
+		return cantidadForro;
+	}
+
+	public void setCantidadForro(String cantidadForro) {
+		this.cantidadForro = cantidadForro;
+	}
+
+	public String getCantidadEntretela() {
+		return cantidadEntretela;
+	}
+
+	public void setCantidadEntretela(String cantidadEntretela) {
+		this.cantidadEntretela = cantidadEntretela;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cantidadEntretela == null) ? 0 : cantidadEntretela.hashCode());
+		result = prime * result + ((cantidadForro == null) ? 0 : cantidadForro.hashCode());
+		result = prime * result + ((cantidadTela == null) ? 0 : cantidadTela.hashCode());
+		result = prime * result + ((idPatronaje == null) ? 0 : idPatronaje.hashCode());
+		result = prime * result + ((idPrenda == null) ? 0 : idPrenda.hashCode());
+		result = prime * result + ((idPrendaPatronaje == null) ? 0 : idPrendaPatronaje.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DisenioPrendaPatronaje other = (DisenioPrendaPatronaje) obj;
+		if (cantidadEntretela == null) {
+			if (other.cantidadEntretela != null)
+				return false;
+		} else if (!cantidadEntretela.equals(other.cantidadEntretela))
+			return false;
+		if (cantidadForro == null) {
+			if (other.cantidadForro != null)
+				return false;
+		} else if (!cantidadForro.equals(other.cantidadForro))
+			return false;
+		if (cantidadTela == null) {
+			if (other.cantidadTela != null)
+				return false;
+		} else if (!cantidadTela.equals(other.cantidadTela))
+			return false;
+		if (idPatronaje == null) {
+			if (other.idPatronaje != null)
+				return false;
+		} else if (!idPatronaje.equals(other.idPatronaje))
+			return false;
+		if (idPrenda == null) {
+			if (other.idPrenda != null)
+				return false;
+		} else if (!idPrenda.equals(other.idPrenda))
+			return false;
+		if (idPrendaPatronaje == null) {
+			if (other.idPrendaPatronaje != null)
+				return false;
+		} else if (!idPrendaPatronaje.equals(other.idPrendaPatronaje))
+			return false;
+		return true;
+	}
+}
