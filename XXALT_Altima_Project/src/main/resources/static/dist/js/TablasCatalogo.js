41be9c52a3837ba395a598f982a732781f43b4b2
kamudoce <59659640+kamudoce@users.noreply.github.com>
2020-03-25 11:45:09 -0600

    Catalago Fix

--- XXALT_Altima_Project/src/main/resources/static/dist/js/TablasCatalogo.js
+++ XXALT_Altima_Project/src/main/resources/static/dist/js/TablasCatalogo.js
@@ -35,13 +35,4 @@ $(document).ready(function() {
             }
         }
     });
-    $('a.toggle-vis').on( 'click', function (e) {
-        e.preventDefault();
- 
-        // Get the column API object
-        var column = table.column( $(this).attr('data-column') );
- 
-        // Toggle the visibility
-        column.visible( ! column.visible() );
-    } );
 });
