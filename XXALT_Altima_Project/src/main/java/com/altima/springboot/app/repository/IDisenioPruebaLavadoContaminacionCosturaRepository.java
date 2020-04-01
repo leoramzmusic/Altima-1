864881eac7220160cdd7375922732a7af55a35c5
victorgarc1a <47542035+victorgarc1a@users.noreply.github.com>
2020-04-01 09:24:08 -0600

    Add files via upload
    repository faltante

---
+++ XXALT_Altima_Project/src/main/java/com/altima/springboot/app/repository/IDisenioPruebaLavadoContaminacionCosturaRepository.java
@@ -0,0 +1,9 @@
+package com.altima.springboot.app.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.altima.springboot.app.models.entity.DisenioPruebaLavadoContaminacionCostura;
+
+public interface IDisenioPruebaLavadoContaminacionCosturaRepository extends CrudRepository<DisenioPruebaLavadoContaminacionCostura, Long> {
+
+}
