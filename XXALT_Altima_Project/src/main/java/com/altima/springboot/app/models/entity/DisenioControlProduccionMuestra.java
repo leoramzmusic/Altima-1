2b9179ebc00ac526ca5bdfc38d7cc148288e397b
DTI <DTI@DESKTOP-AVK6DCT>
2020-04-02 10:54:03 -0600

    Cambio de campos

--- XXALT_Altima_Project/src/main/java/com/altima/springboot/app/models/entity/DisenioControlProduccionMuestra.java
+++ XXALT_Altima_Project/src/main/java/com/altima/springboot/app/models/entity/DisenioControlProduccionMuestra.java
@@ -29,11 +29,11 @@ public class DisenioControlProduccionMuestra implements Serializable {
 	@Column(name="id_pedido")
 	private Long idPedido;
 	
-	@Column(name="fecha_inicio")
-	private String fechaInicio;
+	@Column(name="fecha_recepcion")
+	private String fechaRecepcion;
 	
-	@Column(name="fecha_fin")
-	private String fechaFin;
+	@Column(name="fecha_Entrega")
+	private String fechaEntrega;
 	
 	@Column(name="estatus_tiempo")
 	private String estatusTiempo;
@@ -60,16 +60,6 @@ public class DisenioControlProduccionMuestra implements Serializable {
 	
 	private String ultimaFechaModificacion;
 
-	
-	
-	public String getEstatusTiempo() {
-		return estatusTiempo;
-	}
-
-	public void setEstatusTiempo(String estatusTiempo) {
-		this.estatusTiempo = estatusTiempo;
-	}
-
 	public Long getIdControlProduccionMuestra() {
 		return idControlProduccionMuestra;
 	}
@@ -86,20 +76,28 @@ public class DisenioControlProduccionMuestra implements Serializable {
 		this.idPedido = idPedido;
 	}
 
-	public String getFechaInicio() {
-		return fechaInicio;
+	public String getFechaRecepcion() {
+		return fechaRecepcion;
 	}
 
-	public void setFechaInicio(String fechaInicio) {
-		this.fechaInicio = fechaInicio;
+	public void setFechaRecepcion(String fechaRecepcion) {
+		this.fechaRecepcion = fechaRecepcion;
 	}
 
-	public String getFechaFin() {
-		return fechaFin;
+	public String getFechaEntrega() {
+		return fechaEntrega;
 	}
 
-	public void setFechaFin(String fechaFin) {
-		this.fechaFin = fechaFin;
+	public void setFechaEntrega(String fechaEntrega) {
+		this.fechaEntrega = fechaEntrega;
+	}
+
+	public String getEstatusTiempo() {
+		return estatusTiempo;
+	}
+
+	public void setEstatusTiempo(String estatusTiempo) {
+		this.estatusTiempo = estatusTiempo;
 	}
 
 	public String getIdOperario() {
@@ -153,87 +151,6 @@ public class DisenioControlProduccionMuestra implements Serializable {
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((actualizadoPor == null) ? 0 : actualizadoPor.hashCode());
-		result = prime * result + ((creadoPor == null) ? 0 : creadoPor.hashCode());
-		result = prime * result + ((fechaCreacion == null) ? 0 : fechaCreacion.hashCode());
-		result = prime * result + ((fechaFin == null) ? 0 : fechaFin.hashCode());
-		result = prime * result + ((fechaInicio == null) ? 0 : fechaInicio.hashCode());
-		result = prime * result + ((idControlProduccionMuestra == null) ? 0 : idControlProduccionMuestra.hashCode());
-		result = prime * result + ((idOperario == null) ? 0 : idOperario.hashCode());
-		result = prime * result + ((idPedido == null) ? 0 : idPedido.hashCode());
-		result = prime * result + ((tipo == null) ? 0 : tipo.hashCode());
-		result = prime * result + ((ultimaFechaModificacion == null) ? 0 : ultimaFechaModificacion.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		DisenioControlProduccionMuestra other = (DisenioControlProduccionMuestra) obj;
-		if (actualizadoPor == null) {
-			if (other.actualizadoPor != null)
-				return false;
-		} else if (!actualizadoPor.equals(other.actualizadoPor))
-			return false;
-		if (creadoPor == null) {
-			if (other.creadoPor != null)
-				return false;
-		} else if (!creadoPor.equals(other.creadoPor))
-			return false;
-		if (fechaCreacion == null) {
-			if (other.fechaCreacion != null)
-				return false;
-		} else if (!fechaCreacion.equals(other.fechaCreacion))
-			return false;
-		if (fechaFin == null) {
-			if (other.fechaFin != null)
-				return false;
-		} else if (!fechaFin.equals(other.fechaFin))
-			return false;
-		if (fechaInicio == null) {
-			if (other.fechaInicio != null)
-				return false;
-		} else if (!fechaInicio.equals(other.fechaInicio))
-			return false;
-		if (idControlProduccionMuestra == null) {
-			if (other.idControlProduccionMuestra != null)
-				return false;
-		} else if (!idControlProduccionMuestra.equals(other.idControlProduccionMuestra))
-			return false;
-		if (idOperario == null) {
-			if (other.idOperario != null)
-				return false;
-		} else if (!idOperario.equals(other.idOperario))
-			return false;
-		if (idPedido == null) {
-			if (other.idPedido != null)
-				return false;
-		} else if (!idPedido.equals(other.idPedido))
-			return false;
-		if (tipo == null) {
-			if (other.tipo != null)
-				return false;
-		} else if (!tipo.equals(other.tipo))
-			return false;
-		if (ultimaFechaModificacion == null) {
-			if (other.ultimaFechaModificacion != null)
-				return false;
-		} else if (!ultimaFechaModificacion.equals(other.ultimaFechaModificacion))
-			return false;
-		return true;
-	}
-	
-	
 	
 
 }
