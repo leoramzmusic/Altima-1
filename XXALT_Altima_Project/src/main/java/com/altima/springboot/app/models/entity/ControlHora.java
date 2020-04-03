6ed03d3d4e236bcf85464131f673e61a1d702638
DTI <DTI@DESKTOP-AVK6DCT>
2020-04-03 11:47:32 -0600

    Cambio de nombre de entidades y creacin de nueva entidad

---
+++ XXALT_Altima_Project/src/main/java/com/altima/springboot/app/models/entity/ControlHora.java
@@ -0,0 +1,98 @@
+package com.altima.springboot.app.models.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "alt_control_hora")
+public class ControlHora {
+	
+	/**
+	 * nnn}
+	 * nnn
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name="id_control_hora")
+	@GeneratedValue(strategy=GenerationType.AUTO, generator="native")
+	@GenericGenerator(name="native",strategy="native")
+	private Long idControlHora;
+	
+	@Column(name="id_control_produccion_muestra")
+	private Long idControlProduccionMuestra;
+	
+	@Column(name="fecha_inicio")
+	private String fechaInicio;
+	
+	@Column(name="fecha_fin")
+	private String fechaFin;
+	
+	@Column(name="estatus")
+	private String estatus;
+	
+	@Column(name="tipo")
+	private String tipo;
+
+	public Long getIdControlHora() {
+		return idControlHora;
+	}
+
+	public void setIdControlHora(Long idControlHora) {
+		this.idControlHora = idControlHora;
+	}
+
+	public Long getIdControlProduccionMuestra() {
+		return idControlProduccionMuestra;
+	}
+
+	public void setIdControlProduccionMuestra(Long idControlProduccionMuestra) {
+		this.idControlProduccionMuestra = idControlProduccionMuestra;
+	}
+
+	public String getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(String fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public String getFechaFin() {
+		return fechaFin;
+	}
+
+	public void setFechaFin(String fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+
+	public String getEstatus() {
+		return estatus;
+	}
+
+	public void setEstatus(String estatus) {
+		this.estatus = estatus;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+	
+
+}
---
+++ XXALT_Altima_Project/src/main/java/com/altima/springboot/app/models/entity/Pedido.java
@@ -0,0 +1,95 @@
+package com.altima.springboot.app.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "alt_pedido")
+public class Pedido implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="id_pedido")
+	@GeneratedValue(strategy=GenerationType.AUTO, generator="native")
+	@GenericGenerator(name="native",strategy="native")
+	private Long idPedido;
+	
+	@Column(name="id_cliente")
+	private Long idCliente;
+	
+	@Column(name="fecha_creacion_pedido")
+	private String fechaCreacionPedido;
+	
+	@Column(name="quien_creo__pedido")
+	private String QuienCreoPedido;
+	
+	@Column(name="estatus")
+	private String estatus;
+	
+	@Column(name="descripcion_pedido")
+	private String descripcionPedido;
+
+	public Long getIdPedido() {
+		return idPedido;
+	}
+
+	public void setIdPedido(Long idPedido) {
+		this.idPedido = idPedido;
+	}
+
+	public Long getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Long idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public String getFechaCreacionPedido() {
+		return fechaCreacionPedido;
+	}
+
+	public void setFechaCreacionPedido(String fechaCreacionPedido) {
+		this.fechaCreacionPedido = fechaCreacionPedido;
+	}
+
+	public String getQuienCreoPedido() {
+		return QuienCreoPedido;
+	}
+
+	public void setQuienCreoPedido(String quienCreoPedido) {
+		QuienCreoPedido = quienCreoPedido;
+	}
+
+	public String getEstatus() {
+		return estatus;
+	}
+
+	public void setEstatus(String estatus) {
+		this.estatus = estatus;
+	}
+
+	public String getDescripcionPedido() {
+		return descripcionPedido;
+	}
+
+	public void setDescripcionPedido(String descripcionPedido) {
+		this.descripcionPedido = descripcionPedido;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+}
