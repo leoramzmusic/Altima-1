ad7daea25266e54c976608e0151b2e21491e10e5
warriorx72 <warriorx72@gmail.com>
2020-04-03 11:07:54 -0600

    order by asc

--- XXALT_Altima_Project/src/main/java/com/altima/springboot/app/models/service/DisenioMaterialServiceImpl.java
+++ XXALT_Altima_Project/src/main/java/com/altima/springboot/app/models/service/DisenioMaterialServiceImpl.java
@@ -59,7 +59,7 @@ public class DisenioMaterialServiceImpl implements IDisenioMaterialService {
 	@Transactional
 	public List<DisenioLookup> findListaLookupMat(){
 	
-		return em.createQuery("SELECT idLookup, nombreLookup, tipoLookup FROM DisenioLookup WHERE tipoLookup= 'Material' and   estatus=1").getResultList();
+		return em.createQuery("SELECT idLookup, nombreLookup, tipoLookup FROM DisenioLookup WHERE tipoLookup= 'Material' and   estatus=1 order by nombreLookup").getResultList();
 	}
 	
 	
@@ -69,7 +69,7 @@ public class DisenioMaterialServiceImpl implements IDisenioMaterialService {
 	@Transactional
 	public List<DisenioLookup> findListaLookupMed(){
 	
-		return em.createQuery("SELECT idLookup, nombreLookup, tipoLookup FROM DisenioLookup WHERE tipoLookup= 'Unidad Medida' and   estatus=1").getResultList();
+		return em.createQuery("SELECT idLookup, nombreLookup, tipoLookup FROM DisenioLookup WHERE tipoLookup= 'Unidad Medida' and   estatus=1 order by nombreLookup").getResultList();
 	}
 	
 	
@@ -78,7 +78,7 @@ public class DisenioMaterialServiceImpl implements IDisenioMaterialService {
 	@Transactional
 	public List<DisenioLookup> findListaMarcas(){
 	
-		return em.createQuery("SELECT idLookup, nombreLookup, tipoLookup FROM DisenioLookup WHERE tipoLookup= 'Marca' and   estatus=1").getResultList();
+		return em.createQuery("SELECT idLookup, nombreLookup, tipoLookup FROM DisenioLookup WHERE tipoLookup= 'Marca' and   estatus=1 order by nombreLookup").getResultList();
 	}
 	
 	
@@ -88,7 +88,7 @@ public class DisenioMaterialServiceImpl implements IDisenioMaterialService {
 	@Transactional
 	public List<DisenioLookup> findListaClasificacion(){
 	
-		return em.createQuery("SELECT idLookup, nombreLookup, tipoLookup FROM DisenioLookup WHERE tipoLookup= 'Clasificacion' and   estatus=1").getResultList();
+		return em.createQuery("SELECT idLookup, nombreLookup, tipoLookup FROM DisenioLookup WHERE tipoLookup= 'Clasificacion' and   estatus=1 order by nombreLookup").getResultList();
 	}
 	
 	
@@ -109,13 +109,13 @@ public class DisenioMaterialServiceImpl implements IDisenioMaterialService {
 	@Override
 	public List<DisenioLookup> findLookUps() {
 		// TODO Auto-generated method stub
-		return em.createNativeQuery("SELECT * FROM alt_disenio_lookup WHERE tipo_lookup = 'Pieza Trazo' AND estatus = 1;").getResultList();
+		return em.createNativeQuery("SELECT * FROM alt_disenio_lookup WHERE tipo_lookup = 'Pieza Trazo' AND estatus = 1 order by nombreLookup;").getResultList();
 	}
 
 	@Override
 	public Object findLookUp(Long id) {
 		// TODO Auto-generated method stubString
-		return (Object) em.createNativeQuery("SELECT * FROM alt_disenio_lookup WHERE tipo_lookup = 'Pieza Trazo' AND id_lookup = " + id).getSingleResult();
+		return (Object) em.createNativeQuery("SELECT * FROM alt_disenio_lookup WHERE tipo_lookup = 'Pieza Trazo' AND id_lookup = " + id+" order by nombreLookup").getSingleResult();
 	}
 
 	@SuppressWarnings("unchecked")
@@ -139,7 +139,7 @@ public class DisenioMaterialServiceImpl implements IDisenioMaterialService {
 	@Transactional
 	public List<DisenioLookup> findListaColor(){
 	
-		return em.createQuery("SELECT idLookup, nombreLookup, tipoLookup FROM DisenioLookup WHERE tipoLookup= 'Color' and   estatus=1").getResultList();
+		return em.createQuery("SELECT idLookup, nombreLookup, tipoLookup FROM DisenioLookup WHERE tipoLookup= 'Color' and   estatus=1 order by nombreLookup").getResultList();
 	}
    
    @SuppressWarnings("unchecked")
@@ -147,7 +147,7 @@ public class DisenioMaterialServiceImpl implements IDisenioMaterialService {
 	@Transactional
 	public List<DisenioLookup> findListaLookupPro(){
 	
-		return em.createQuery("SELECT idLookup, nombreLookup, tipoLookup FROM DisenioLookup WHERE tipoLookup= 'Proceso' and   estatus=1").getResultList();
+		return em.createQuery("SELECT idLookup, nombreLookup, tipoLookup FROM DisenioLookup WHERE tipoLookup= 'Proceso' and   estatus=1 order by nombreLookup").getResultList();
 	}
 
 
@@ -170,14 +170,14 @@ public class DisenioMaterialServiceImpl implements IDisenioMaterialService {
 	@Override
 	public List<DisenioLookup> findAllFamiliaPrenda() {
 		// TODO Auto-generated method stub
-		return em.createQuery("SELECT idLookup, nombreLookup, tipoLookup FROM DisenioLookup WHERE tipoLookup= 'Familia Prenda' and   estatus=1").getResultList();
+		return em.createQuery("SELECT idLookup, nombreLookup, tipoLookup FROM DisenioLookup WHERE tipoLookup= 'Familia Prenda' and   estatus=1 order by nombreLookup").getResultList();
 	}	
 	
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<Object> findAllByTipoMaterial(Long id) {
 		// TODO Auto-generated method stub
-		return em.createQuery("SELECT idMaterial, nombreMaterial FROM DisenioMaterial WHERE idTipoMaterial="+id+" AND estatus = 1").getResultList();
+		return em.createQuery("SELECT idMaterial, nombreMaterial FROM DisenioMaterial WHERE idTipoMaterial="+id+" AND estatus = 1 order").getResultList();
 	}	
 
 }
