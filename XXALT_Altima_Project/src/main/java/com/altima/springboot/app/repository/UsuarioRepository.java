348140994a007df5aebf5da9a5c3f0c8c5053426
warriorx72 <warriorx72@gmail.com>
2020-04-06 09:18:40 -0500

    Cambios usuario

--- XXALT_Altima_Project/src/main/java/com/altima/springboot/app/repository/UsuarioRepository.java
+++ XXALT_Altima_Project/src/main/java/com/altima/springboot/app/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.altima.springboot.app.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,7 +9,7 @@ import com.altima.springboot.app.models.entity.Usuario;
 
 @Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, Long>{
-	
-	public Usuario findBynombreUsuario(String username);
+	//cambio
+	public Optional<Usuario> findBynombreUsuario(String username); 
 
 }
