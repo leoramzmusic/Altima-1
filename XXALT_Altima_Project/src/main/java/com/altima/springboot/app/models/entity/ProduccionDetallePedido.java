49852290ea957640b1c3bd65dcb1e637a91b9e0f
DTI <DTI@DESKTOP-AVK6DCT>
2020-04-14 10:26:15 -0500

    Se cambio nombre de esta entidad

---
+++ XXALT_Altima_Project/src/main/java/com/altima/springboot/app/models/entity/ProduccionDetallePedido.java
@@ -0,0 +1,164 @@
+package com.altima.springboot.app.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "alt_produccion_orden")
+public class ProduccionOrden implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name="id_orden")
+	@GeneratedValue(strategy=GenerationType.AUTO, generator="native")
+	@GenericGenerator(name="native",strategy="native")
+	private Long idOrden;
+	
+	@Column(name="id_pedido")
+	private Long idPedido;
+	
+	@Column(name="id_tela")
+	private Long idTela;
+	
+	@Column(name="talla")
+	private String talla;
+	
+	@Column(name="largo")
+	private String largo;
+	
+	@Column(name="descripcion")
+	private String descripcion;
+	
+	@Column(name="id_text")
+	private String idText;
+	
+	@Column(name="creado_por")
+	private String creadoPor;
+	
+	@Column(name="actualizado_por")
+	private String actualizadoPor;
+	
+	@Column(name="fecha_creacion")
+	private String fechaCreacion;
+	
+	@Column(name="ultima_fecha_modificacion")
+	private String ultimaFechaModificacion;
+	
+	@Column(name="estatus")
+	private String estatus;
+	
+
+	public Long getIdTela() {
+		return idTela;
+	}
+
+	public void setIdTela(Long idTela) {
+		this.idTela = idTela;
+	}
+
+	public String getTalla() {
+		return talla;
+	}
+
+	public void setTalla(String talla) {
+		this.talla = talla;
+	}
+
+	public String getLargo() {
+		return largo;
+	}
+
+	public void setLargo(String largo) {
+		this.largo = largo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Long getIdOrden() {
+		return idOrden;
+	}
+
+	public void setIdOrden(Long idOrden) {
+		this.idOrden = idOrden;
+	}
+
+	public Long getIdPedido() {
+		return idPedido;
+	}
+
+	public void setIdPedido(Long idPedido) {
+		this.idPedido = idPedido;
+	}
+
+	public String getIdText() {
+		return idText;
+	}
+
+	public void setIdText(String idText) {
+		this.idText = idText;
+	}
+
+	public String getCreadoPor() {
+		return creadoPor;
+	}
+
+	public void setCreadoPor(String creadoPor) {
+		this.creadoPor = creadoPor;
+	}
+
+	public String getActualizadoPor() {
+		return actualizadoPor;
+	}
+
+	public void setActualizadoPor(String actualizadoPor) {
+		this.actualizadoPor = actualizadoPor;
+	}
+
+	public String getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(String fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public String getUltimaFechaModificacion() {
+		return ultimaFechaModificacion;
+	}
+
+	public void setUltimaFechaModificacion(String ultimaFechaModificacion) {
+		this.ultimaFechaModificacion = ultimaFechaModificacion;
+	}
+
+	public String getEstatus() {
+		return estatus;
+	}
+
+	public void setEstatus(String estatus) {
+		this.estatus = estatus;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+
+}
