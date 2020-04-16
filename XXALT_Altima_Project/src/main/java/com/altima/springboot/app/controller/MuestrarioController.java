3793cd786792776c4211a34078f51874bdafa42d
LeonardoTiDev <60356628+LeonardoTiDev@users.noreply.github.com>
2020-04-16 12:57:54 -0500

    pantalla de inventario

--- XXALT_Altima_Project/src/main/java/com/altima/springboot/app/controller/MuestrarioController.java
+++ XXALT_Altima_Project/src/main/java/com/altima/springboot/app/controller/MuestrarioController.java
@@ -1,14 +1,31 @@
 package com.altima.springboot.app.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.altima.springboot.app.models.entity.ProduccionDetallePedido;
+import com.altima.springboot.app.models.service.IInventarioService;
 
 @Controller
 public class MuestrarioController {
+	
+	@Autowired
+	private IInventarioService inventario;
+	
+	
 	@GetMapping("/inventario")
-	public String Inventario() {
+	public String Inventario(Model model) {
+		
+		List<ProduccionDetallePedido> listInventario= inventario.listInventario();
+		model.addAttribute("listInventario", listInventario);
+		System.out.println("Si esta entrando al controller");	
 		return "inventario";
 	}
+
 	
 	@GetMapping("/agregar-inventario")
 	public String agregarInventario() {
