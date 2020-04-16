bf65e0299866c7466063b8bdf057cde756cdc6cf
carlos151195 <eliosaamarocarlos@gmail.com>
2020-04-16 12:53:08 -0500

    Update CatalogoServiceImpl.java

--- XXALT_Altima_Project/src/main/java/com/altima/springboot/app/models/service/CatalogoServiceImpl.java
+++ XXALT_Altima_Project/src/main/java/com/altima/springboot/app/models/service/CatalogoServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.altima.springboot.app.models.entity.DisenioLookup;
 import com.altima.springboot.app.repository.CatalogoRepository;
 
+
 @Service
 public class CatalogoServiceImpl implements ICatalogoService {
 	@PersistenceContext
@@ -20,29 +21,28 @@ public class CatalogoServiceImpl implements ICatalogoService {
 	
 	@Autowired
 	private CatalogoRepository repository;
-	
 	@Override
-	@Transactional(readOnly = true)
+	@Transactional(readOnly=true)
 	public List<DisenioLookup> findAll() {
 		// TODO Auto-generated method stub
 		return (List<DisenioLookup>) repository.findAll();
 	}
-	
+
 	@Override
 	@Transactional
 	public void save(DisenioLookup diseniolookup) {
 		// TODO Auto-generated method stub
 		repository.save(diseniolookup);
-		
+
 	}
-	
+
 	@Override
 	@Transactional
 	public void delete(Long id) {
 		// TODO Auto-generated method stub
 		repository.deleteById(id);
 	}
-	
+
 	@Override
 	@Transactional
 	public DisenioLookup findOne(Long id) {
@@ -50,12 +50,12 @@ public class CatalogoServiceImpl implements ICatalogoService {
 		return repository.findById(id).orElse(null);
 	}
 	
+	
 	@SuppressWarnings("unchecked")
 	@Override
 	@Transactional
 	public List<DisenioLookup> findAllMarca() {
-		return em.createQuery("from DisenioLookup where tipo_lookup='Marca' and Estatus=1 ORDER BY idLookup ASC")
-				.getResultList();
+		return em.createQuery("from DisenioLookup where tipo_lookup='Marca' and Estatus=1 ORDER BY idLookup ASC").getResultList();
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -97,110 +97,113 @@ public class CatalogoServiceImpl implements ICatalogoService {
 	@SuppressWarnings("unchecked")
 	@Override
 	@Transactional
-	public List<DisenioLookup> findAllInstrCuidado() {
+	public List<DisenioLookup> findAllInstrCuidado(){
 		return em.createQuery("from DisenioLookup where tipo_lookup='Instruccion Cuidado' and Estatus=1").getResultList();
 	}
 	
 	@SuppressWarnings("unchecked")
 	@Override
 	@Transactional
-	public List<DisenioLookup> findAllUnidadMedida() {
+	public List<DisenioLookup> findAllUnidadMedida(){
 		return em.createQuery("from DisenioLookup where tipo_lookup='Unidad Medida' and Estatus=1").getResultList();
 	}
 	
 	@SuppressWarnings("unchecked")
 	@Override
 	@Transactional
-	public List<DisenioLookup> findAllMaterial() {
+	public List<DisenioLookup> findAllMaterial(){
 		return em.createQuery("from DisenioLookup where tipo_lookup='Material' and Estatus=1").getResultList();
 	}
 	
 	@SuppressWarnings("unchecked")
 	@Override
 	@Transactional
-	public List<DisenioLookup> findAllMarcador() {
+	public List<DisenioLookup> findAllMarcador(){
 		return em.createQuery("from DisenioLookup where tipo_lookup='Marcador' and Estatus=1").getResultList();
 	}
 	
 	@SuppressWarnings("unchecked")
 	@Override
 	@Transactional
-	public List<DisenioLookup> findAllComposicion() {
+	public List<DisenioLookup> findAllComposicion(){
 		return em.createQuery("from DisenioLookup where tipo_lookup='Composicion' and Estatus=1").getResultList();
 	}
 	
 	@Override
 	@Transactional
-	public DisenioLookup findLastComposicion() {
-		return (DisenioLookup) em.createQuery("from DisenioLookup where tipo_lookup='Composicion' ORDER BY idLookup DESC")
-				.setMaxResults(1).getSingleResult();
+	public DisenioLookup findLastComposicion(){
+		return (DisenioLookup) em.createQuery("from DisenioLookup where tipo_lookup='Composicion' ORDER BY idLookup DESC").setMaxResults(1).getSingleResult();
 	}
 	
 	@Override
 	@Transactional
-	public DisenioLookup findLastMarcador() {
-		return (DisenioLookup) em.createQuery("from DisenioLookup where tipo_lookup='Marcador' ORDER BY idLookup DESC")
-				.setMaxResults(1).getSingleResult();
+	public DisenioLookup findLastMarcador(){
+		return (DisenioLookup) em.createQuery("from DisenioLookup where tipo_lookup='Marcador' ORDER BY idLookup DESC").setMaxResults(1).getSingleResult();
 	}
 	
 	@Override
 	@Transactional
-	public DisenioLookup findLastMaterial() {
-		return (DisenioLookup) em.createQuery("from DisenioLookup where tipo_lookup='Material' ORDER BY idLookup DESC")
-				.setMaxResults(1).getSingleResult();
+	public DisenioLookup findLastMaterial(){
+		return (DisenioLookup) em.createQuery("from DisenioLookup where tipo_lookup='Material' ORDER BY idLookup DESC").setMaxResults(1).getSingleResult();
 	}
 	
 	@Override
 	@Transactional
-	public DisenioLookup findLastUnidadMedida() {
-		return (DisenioLookup) em.createQuery("from DisenioLookup where tipo_lookup='Unidad Medida' ORDER BY idLookup DESC")
-				.setMaxResults(1).getSingleResult();
+	public DisenioLookup findLastUnidadMedida(){
+		return (DisenioLookup) em.createQuery("from DisenioLookup where tipo_lookup='Unidad Medida' ORDER BY idLookup DESC").setMaxResults(1).getSingleResult();
 	}
 	
 	@Override
 	@Transactional
-	public DisenioLookup findLastInstrCuidado() {
-		return (DisenioLookup) em
-				.createQuery("from DisenioLookup where tipo_lookup='Instruccion Cuidado' ORDER BY idLookup DESC")
-				.setMaxResults(1).getSingleResult();
+	public DisenioLookup findLastInstrCuidado(){
+		return (DisenioLookup) em.createQuery("from DisenioLookup where tipo_lookup='Instruccion Cuidado' ORDER BY idLookup DESC").setMaxResults(1).getSingleResult();
 	}
 	
 	@Override
 	@Transactional
-	public DisenioLookup findLastFamComposicion() {
-		return (DisenioLookup) em
-				.createQuery("from DisenioLookup where tipo_lookup='Familia Composicion' ORDER BY idLookup DESC")
-				.setMaxResults(1).getSingleResult();
+	public DisenioLookup findLastFamComposicion(){
+		return (DisenioLookup) em.createQuery("from DisenioLookup where tipo_lookup='Familia Composicion' ORDER BY idLookup DESC").setMaxResults(1).getSingleResult();
 	}
 	
 	@Override
 	@Transactional
-	public DisenioLookup findLastFamGenero() {
-		return (DisenioLookup) em
-				.createQuery("from DisenioLookup where tipo_lookup='Familia Genero' ORDER BY idLookup DESC").setMaxResults(1)
-				.getSingleResult();
+	public DisenioLookup findLastFamGenero(){
+		return (DisenioLookup) em.createQuery("from DisenioLookup where tipo_lookup='Familia Genero' ORDER BY idLookup DESC").setMaxResults(1).getSingleResult();
 	}
 	
 	@Override
 	@Transactional
-	public DisenioLookup findLastFamPrendas() {
-		return (DisenioLookup) em
-				.createQuery("from DisenioLookup where tipo_lookup='Familia Prenda' ORDER BY idLookup DESC").setMaxResults(1)
-				.getSingleResult();
+	public DisenioLookup findLastFamPrendas(){
+		return (DisenioLookup) em.createQuery("from DisenioLookup where tipo_lookup='Familia Prenda' ORDER BY idLookup DESC").setMaxResults(1).getSingleResult();
 	}
 	
 	@Override
 	@Transactional
-	public DisenioLookup findLastPzasTrazo() {
-		return (DisenioLookup) em.createQuery("from DisenioLookup where tipo_lookup='Pieza Trazo' ORDER BY idLookup DESC")
-				.setMaxResults(1).getSingleResult();
+	public DisenioLookup findLastPzasTrazo(){
+		return (DisenioLookup) em.createQuery("from DisenioLookup where tipo_lookup='Pieza Trazo' ORDER BY idLookup DESC").setMaxResults(1).getSingleResult();
 	}
 	
 	@Override
 	@Transactional
-	public DisenioLookup findLastColor() {
-		return (DisenioLookup) em.createQuery("from DisenioLookup where tipo_lookup='Color' ORDER BY idLookup DESC")
-				.setMaxResults(1).getSingleResult();
+	public DisenioLookup findLastColor(){
+		return (DisenioLookup) em.createQuery("from DisenioLookup where tipo_lookup='Color' ORDER BY idLookup DESC").setMaxResults(1).getSingleResult();
 	}
 	
+	@Override
+	@Transactional
+	public boolean findDuplicate(String Lookup){
+		boolean duplicate;
+		@SuppressWarnings("unchecked")
+		List<DisenioLookup> result = em.createQuery("from DisenioLookup where nombreLookup='"+Lookup+"'").getResultList();
+		if(result.isEmpty()) {
+			duplicate=false;
+		}
+		else {
+			duplicate=true;
+		}
+		 return duplicate;
+	}
+	
+	
+
 }
