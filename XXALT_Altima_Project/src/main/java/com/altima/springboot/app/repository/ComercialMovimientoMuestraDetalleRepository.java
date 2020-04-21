f74e71b86ecdb134e69c19cbc52de65768a81553
victorgarc1a <47542035+victorgarc1a@users.noreply.github.com>
2020-04-21 08:03:36 -0500

    Add files via upload
    repositories

---
+++ XXALT_Altima_Project/src/main/java/com/altima/springboot/app/repository/ComercialMovimientoMuestraDetalleRepository.java
@@ -0,0 +1,9 @@
+package com.altima.springboot.app.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.altima.springboot.app.models.entity.ComercialMovimientoMuestraDetalle;
+
+public interface ComercialMovimientoMuestraDetalleRepository extends CrudRepository< ComercialMovimientoMuestraDetalle, Long> {
+
+}
