cfa0a2db61824cbc8935a9a067956045ed33c632
TIErik <55719391+TIErik@users.noreply.github.com>
2020-04-22 11:32:52 -0500

    Add files via upload

---
+++ XXALT_Altima_Project/src/main/java/com/altima/springboot/app/repository/ProduccionPedidoRepository.java
@@ -0,0 +1,7 @@
+package com.altima.springboot.app.repository;
+import org.springframework.data.repository.CrudRepository;
+import com.altima.springboot.app.models.entity.ProduccionPedido;
+public interface ProduccionPedidoRepository extends CrudRepository<ProduccionPedido, Long> {
+
+	// 
+}
