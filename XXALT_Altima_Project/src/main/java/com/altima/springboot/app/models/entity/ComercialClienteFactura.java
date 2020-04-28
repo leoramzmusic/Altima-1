d6aff429a3f448d0515efd0da6b444766921b78e
DTI <DTI@DESKTOP-AVK6DCT>
2020-04-28 12:58:15 -0500

    se agrega otro campos :)  ( se mos olvido) jajja

--- XXALT_Altima_Project/src/main/java/com/altima/springboot/app/models/entity/ComercialClienteFactura.java
+++ XXALT_Altima_Project/src/main/java/com/altima/springboot/app/models/entity/ComercialClienteFactura.java
@@ -66,6 +66,20 @@ public class ComercialClienteFactura implements Serializable{
 	@Column(name="rfc_factura")
 	private String rfcFactura;
 	
+	@Column(name="razon_social")
+	private String razon_social;
+	
+	
+	
+	
+	public String getRazon_social() {
+		return razon_social;
+	}
+
+	public void setRazon_social(String razon_social) {
+		this.razon_social = razon_social;
+	}
+
 	public Long getIdClienteFactura() {
 		return idClienteFactura;
 	}
