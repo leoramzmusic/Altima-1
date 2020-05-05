e9097a07f73667d3d1317dcfee847a1677e711c4
TIErik <55719391+TIErik@users.noreply.github.com>
2020-05-05 16:01:43 -0500

    Add files via upload

---
+++ XXALT_Altima_Project/src/main/java/com/altima/springboot/app/repository/ComercialCalendarioRepository.java
@@ -0,0 +1,6 @@
+package com.altima.springboot.app.repository;
+import org.springframework.data.repository.CrudRepository;
+import com.altima.springboot.app.models.entity.ComercialCalendario;
+public interface ComercialCalendarioRepository extends CrudRepository<ComercialCalendario, Long> {
+
+}
